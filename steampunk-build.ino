0198098b0893bd18095b1b6fafab58292479d157
Lonny Gomes <code@lonnygomes.com>
2021-01-20 00:59:58 -0500

    feat: add servo updates  for temp/humid gauges

--- steampunk-build.ino
+++ steampunk-build.ino
@@ -1,36 +1,64 @@
+#include <Servo.h>
 #include "DHT.h"
 
-#define DHT_TYPE DHT22
+// the delay for each loop cyle
+#define REFRESH_RATE 2000
+// type of temperature sensor
+#define DHT_TYPE DHT22 
+// data pin for temperature sensor
 #define DHT_PIN 2
+// data pins for servos
+#define SERVO_PIN_TEMP 3
+#define SERVO_PIN_HUMID 4
 
 // library instance definitions
 DHT dht(DHT_PIN, DHT_TYPE);
+Servo servoTemp;
+Servo servoHumid;
 
+// variable definitions
+float temperature;
+float humidity;
+int servoValTemp;
+int servoValHumid;
 
 void setup() {
   Serial.begin(9600);
-  
+
+  // start monitoring temperature/humidity
   dht.begin();
+
+  // attach to servos
+  servoTemp.attach(SERVO_PIN_TEMP);
+  servoHumid.attach(SERVO_PIN_HUMID);
 }
 
 void loop() {
   Serial.println();
 
-  float humidity = dht.readHumidity(); 
-  float temperature = dht.readTemperature(true);
-  
-  
+  humidity = dht.readHumidity(); 
+  temperature = dht.readTemperature(true);
+
   if (isnan(humidity) || isnan(temperature)) {
     Serial.println(F("Failed to read from DHT sensor!"));
-    delay(2000);
+    delay(REFRESH_RATE);
     return;
   }
 
+  // calculate servo positions based on temp/humid values
+  // NOTE: servo ranges must be flipped because of thier rotation
+  int servoPosHumid = map(humidity, 0, 100, 180, 0);
+  int servoPosTemp = map(temperature, 0, 100, 180, 0);
+  servoHumid.write(servoPosHumid);
+  servoTemp.write(servoPosTemp);  
+
+  Serial.print(servoPosTemp);
+  Serial.print("----");
   Serial.print("Humidity (%): ");
   Serial.println((float)humidity, 2);
 
   Serial.print("Temperature (F): ");
   Serial.println(temperature);
 
-  delay(2000);
+  delay(REFRESH_RATE);
 }
